fcf839f20d24157177bf479616eb07b0d0201dbe
Vitali Veys <veysoff@gmail.com>
2023-12-15 15:48:17 +0100

    Golang Server with Gin Gonic

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/veysoff/golang-crud-postgres-server/initializers"
+)
+
+var (
+	server *gin.Engine
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+
+	server = gin.Default()
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		message := "Server is runing. Golang with Gorm and Postgres"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
+	})
+
+	log.Fatal(server.Run(":" + config.ServerPort))
 }
